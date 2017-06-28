e8ccc4b0a29bf30ab177002c023a722813d4cc1d
apapamichalis <dimxer@hotmail.com>
2017-06-28 17:16:21 +0300

    Add model specs

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @list = @user.lists.create(name: "My Awesome List")
+  end
+
+  it "is valid with a user and a list" do 
+    vote = Vote.new(
+      user: @user,
+      list: @list
+    )
+    expect(vote).to be_valid
+  end
+
+  it "is invalid without a list" do 
+    vote = Vote.new(
+      user: @user,
+      list: nil
+    )
+    expect(vote).not_to be_valid
+  end
+
+  it "is invalid without a user" do 
+    vote = Vote.new(
+      user: nil,
+      list: @lsit
+    )
+    expect(vote).not_to be_valid
+  end
 end
