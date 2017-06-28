0f24c243e3123079f1baa9550c819aed6ee28fd4
George V. Bassis <gebassis@outlook.com>
2017-06-28 10:44:20 +0300

    Remove password length check. Aforementioned check already done by devise module.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable (Included specific password length to override the default
-  # value of the devise module (6).)
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, password_length: 8..128
   validates :name, presence: true
