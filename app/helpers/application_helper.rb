54726ab82a868dfbc96c58b224e8c0ed66f59588
apapamichalis <dimxer@hotmail.com>
2017-07-02 09:26:42 +0300

    Lists can now be sorted be name or votes_count or created_at, asc or desc in lists index.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,6 @@ module ApplicationHelper
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
     icon = sort_direction == "asc" ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down"
     icon = column == sort_column ? icon : ""
-    link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
-  
+    link_to "#{title} <span class='#{icon}'></span>".html_safe, {sort: column, column: column, direction: direction}
   end
-
-
 end
